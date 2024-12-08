16f5224c2139df62b43e9d8b7ddf1e8eff963145
Piyush Agarwal <piyushagarwalvo@gmail.com>
2024-12-08 22:03:46 +0530

    fix resend

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,12 +1,6 @@
 import { Resend } from "resend";
 
-const resendApiKey = process.env.RESEND_API_KEY;
-
-if (!resendApiKey) {
-  console.warn("WARNING: RESEND_API_KEY is not set in environment variables");
-}
-
-export const resend = new Resend(resendApiKey);
+export const resend = new Resend(process.env.RESEND_API_KEY || "");
 
 export async function sendEmail({ to, subject, react }) {
   try {
