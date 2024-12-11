525a7a22f2088edffad801f10b37f4047b22208d
Piyush Agarwal <piyushagarwalvo@gmail.com>
2024-12-11 11:35:16 +0530

    image alignment

--- components/hero.jsx
+++ components/hero.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
     <section className="pt-40 pb-20 px-4">
       <div className="container mx-auto text-center">
         <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
-          Manage Your Finances with Intelligence
+          Manage Your Finances <br /> with Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
           An AI-powered financial management platform that helps you track,
@@ -55,7 +55,7 @@ const HeroSection = () => {
               width={1280}
               height={720}
               alt="Dashboard Preview"
-              className="rounded-lg shadow-2xl border"
+              className="rounded-lg shadow-2xl border mx-auto"
               priority
             />
           </div>
